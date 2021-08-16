6934204ea56339f2bf4e93a954e9090fd9457103
FeketeO <69634719+FeketeO@users.noreply.github.com>
2021-08-16 20:28:16 +0200

    default.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,36 @@ const config = require('config');
 const logger = require('./config/logger');
 const app = require('./server');
 const port = process.env.PORT || 3000;
+mongoose.Promise = global.Promise;
 
-// Database connection.
+
+const connectionString =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mcfhb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const options = {
+  useNewUrlParser: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+//Database connection.
 if (!config.has('database')) {
     logger.error('No database config found.');
     process.exit();
 }
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
+
+mongoose.connect(connectionString, options)
+  .then(() => logger.info('MongoDB connection has been established successfully.'))
+  .catch(err => {
+    logger.error(err);
+    process.exit(-1);
+  });
+
+const app = require('./server');
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
